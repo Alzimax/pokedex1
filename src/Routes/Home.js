59f3b7ada52c0c1de2a37d254e0ead6911a9ee0e
Alzimax <90858641+Alzimax@users.noreply.github.com>
2023-04-24 17:28:54 +0200

    Updates

--- src/Routes/Home.js
+++
@@ -1,8 +0,0 @@
-export default function Home(){
-    return (
-        <div>
-            <h1>WELCOME TRAINER!</h1>
-            <p>Welcome to your Pokedex! Here you can research all you want to know about pokemon!</p>
-        </div>
-    )
-}
